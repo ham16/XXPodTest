3c7e03300fd9b623c20d3a149e5eb1907141a1da
ham <huangx@hyxt.com>
2016-09-22 11:48:32 +0800

    提交壳工程到git

---
+++ XXPodObject/XXPodObject.h
@@ -0,0 +1,15 @@
+//
+//  XXPodObject.h
+//  XXPodTest
+//
+//  Created by ham on 16/9/22.
+//  Copyright © 2016年 ham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XXPodObject : NSObject
+
++ (void)testPod;
+
+@end
---
+++ XXPodObject/XXPodObject.m
@@ -0,0 +1,17 @@
+//
+//  XXPodObject.m
+//  XXPodTest
+//
+//  Created by ham on 16/9/22.
+//  Copyright © 2016年 ham. All rights reserved.
+//
+
+#import "XXPodObject.h"
+
+@implementation XXPodObject
+
++ (void)testPod {
+    NSLog(@"OK!");
+}
+
+@end
